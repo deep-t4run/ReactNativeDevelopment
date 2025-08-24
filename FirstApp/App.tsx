4d7a2cd880ac77f9f1760453a7f70ddf8bacf829
deep-t4run <deep.t4run@gmail.com>
2025-08-24 10:07:29 +0530

    Done with the concept of Flex and Positions

--- FirstApp/App.tsx
+++ FirstApp/App.tsx
@@ -2,54 +2,68 @@ import { StyleSheet, SafeAreaView, Platform, View, Text } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.view1}>
-        <Text style={styles.text}>Hello Parth</Text>
-      </View>
+    <View style={styles.container}>
+      <View style={styles.view1}></View>
 
-      <View style={styles.view2}>
-        <View style={styles.view3}></View>
-      </View>
-    </SafeAreaView>
+      <View style={styles.view2}></View>
+
+      <View style={styles.view3}></View>
+
+      <View style={styles.view4}></View>
+{/* 
+      <View style={styles.view5}></View>
+
+      <View style={styles.view6}></View> */}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Platform.OS === "ios" ? "grey" : "aqua",
-    marginTop: 50,
+    flexDirection: "row",
+    // flexWrap: "wrap",
+    // alignContent: "center",
+    backgroundColor: "white",
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "center"
   },
 
   view1: {
-    height: 200,
-    width: 200,
-    backgroundColor: "blue",
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 20,
+    height: 90,
+    width: 90,
+    backgroundColor: "#f17610ff",
   },
 
   view2: {
-    height: 200,
-    width: 200,
-    backgroundColor: "red",
-    padding: 20,
-    // justifyContent: "center",
-    // alignItems: "center",
+    height: 90,
+    width: 90,
+    backgroundColor: "grey",
   },
 
   view3: {
-    height: 30,
-    width: 30,
-    backgroundColor: "black",
+    height: 90,
+    width: 90,
+    backgroundColor: "green",
+    bottom: 20,
+    left: 12
   },
 
-  text: {
-    color: "white",
-    fontSize: 25,
-    fontWeight: "bold",
+  view4: {
+    height: 90,
+    width: 90,
+    backgroundColor: "blue",
   },
+
+  // view5: {
+  //   height: 90,
+  //   width: 90,
+  //   backgroundColor: "red",
+  // },
+
+  // view6: {
+  //   height: 90,
+  //   width: 90,
+  //   backgroundColor: "pink",
+  // },
 });
