733fe1c8cbf094002793587a7ad70b5a3e1b8819
deep-t4run <deep.t4run@gmail.com>
2025-08-25 00:34:11 +0530

    Learnt about useState hook

--- FirstApp/App.tsx
+++ FirstApp/App.tsx
@@ -1,21 +1,30 @@
-import { View } from "react-native";
-import { scale, verticalScale, s, vs } from "react-native-size-matters";
+import { useState } from "react";
+import { Button, View, Text } from "react-native";
 
 export default function App() {
+  const [state, setState] = useState(0);
+
+  const increaseValue = () => {
+    setState(state + 1);
+  };
+
+  const decreaseValue = () => setState(state - 1);
+
+  const resetValue = () => setState(0);
+
   return (
     <View
       style={{
-        backgroundColor: "#fff",
+        backgroundColor: "#white",
         flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <View
-        style={{
-          backgroundColor: "red",
-          width: s(300),
-          height: vs(600),
-        }}
-      ></View>
+      <Button title="Increase" onPress={increaseValue} />
+      <Text style={{ fontSize: 50 }}>{state}</Text>
+      <Button title="Decrease" onPress={decreaseValue} />
+      <Button title="Reset" onPress={resetValue} />
     </View>
   );
 }
