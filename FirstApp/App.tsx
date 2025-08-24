5d05a0ec8bb93e929655a0cc06813e873e01c708
deep-t4run <deep.t4run@gmail.com>
2025-08-24 23:28:03 +0530

    Done with the concept of Dimensions

--- FirstApp/App.tsx
+++ FirstApp/App.tsx
@@ -1,69 +1,25 @@
-import { StyleSheet, SafeAreaView, Platform, View, Text } from "react-native";
+import { View, Dimensions } from "react-native";
 
 export default function App() {
+  console.log(Dimensions.get("screen"));
+
+  const PHONE_WIDTH = Dimensions.get("screen").width;
+  const PHONE_HEIGHT = Dimensions.get("screen").height;
+
   return (
-    <View style={styles.container}>
-      <View style={styles.view1}></View>
-
-      <View style={styles.view2}></View>
-
-      <View style={styles.view3}></View>
-
-      <View style={styles.view4}></View>
-{/* 
-      <View style={styles.view5}></View>
-
-      <View style={styles.view6}></View> */}
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "red",
+          width: PHONE_WIDTH,
+          height: PHONE_HEIGHT/2,
+        }}
+      ></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    // flexWrap: "wrap",
-    // alignContent: "center",
-    backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-
-  view1: {
-    height: 90,
-    width: 90,
-    backgroundColor: "#f17610ff",
-  },
-
-  view2: {
-    height: 90,
-    width: 90,
-    backgroundColor: "grey",
-  },
-
-  view3: {
-    height: 90,
-    width: 90,
-    backgroundColor: "green",
-    bottom: 20,
-    left: 12
-  },
-
-  view4: {
-    height: 90,
-    width: 90,
-    backgroundColor: "blue",
-  },
-
-  // view5: {
-  //   height: 90,
-  //   width: 90,
-  //   backgroundColor: "red",
-  // },
-
-  // view6: {
-  //   height: 90,
-  //   width: 90,
-  //   backgroundColor: "pink",
-  // },
-});
